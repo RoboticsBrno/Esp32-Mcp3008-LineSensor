074eb96db9edbfddbdb175922b78610e2397f8cc
Vojtech Bocek <vbocek@gmail.com>
2019-07-08 17:21:00 +0200

    Split Driver & LineSensor

--- src/linesensor.cpp
+++ src/linesensor.cpp
@@ -7,149 +7,15 @@
 
 namespace mcp3008 {
 
-LineSensor::LineSensor() : m_spi(NULL), m_spi_dev(HSPI_HOST), m_installed(false), m_channels_mask(0xFF) {
+LineSensor::LineSensor() : Driver() {
 }
 
 LineSensor::~LineSensor() {
-    uninstall();
-}
 
-esp_err_t LineSensor::install(const LineSensor::Config& cfg) {
-    if(m_installed)
-        return ESP_OK;
-
-    esp_err_t ret;
-    spi_bus_config_t buscfg = { 0 };
-    buscfg.miso_io_num = cfg.pin_miso;
-    buscfg.mosi_io_num = cfg.pin_mosi;
-    buscfg.sclk_io_num = cfg.pin_sck;
-    buscfg.quadwp_io_num = -1;
-    buscfg.quadhd_io_num = -1;
-
-    spi_device_interface_config_t devcfg = { 0 };
-    devcfg.clock_speed_hz = cfg.freq;
-    devcfg.mode = 0;
-    devcfg.spics_io_num = cfg.pin_cs;
-    devcfg.queue_size = CHANNELS;
-
-    ret = spi_bus_initialize(cfg.spi_dev, &buscfg, 1);
-    if(ret != ESP_OK) {
-        return ret;
-    }
-
-    ret = spi_bus_add_device(cfg.spi_dev, &devcfg, &m_spi);
-    if(ret != ESP_OK) {
-        spi_bus_free(cfg.spi_dev);
-        return ret;
-    }
-
-    m_spi_dev = cfg.spi_dev;
-    m_channels_mask = cfg.channels_mask;
-    m_installed = true;
-    return ESP_OK;
-}
-
-esp_err_t LineSensor::uninstall() {
-    if(!m_installed)
-        return ESP_OK;
-
-    esp_err_t res = spi_bus_remove_device(m_spi);
-    if(res != ESP_OK)
-        return res;
-
-    res = spi_bus_free(m_spi_dev);
-    if(res != ESP_OK)
-        return res;
-
-    m_installed = false;
-    return ESP_OK;
 }
 
 LineSensorCalibrator LineSensor::startCalibration() {
     return LineSensorCalibrator(*this);
-}
-
-int LineSensor::requestToChannel(int request) const {
-    if(m_channels_mask == 0xFF)
-        return request;
-
-    int requested = 0;
-    for(int i = 0; i < CHANNELS; ++i) {
-        if(((1 << i) & m_channels_mask) != 0) {
-            if(requested++ == request)
-                return i;
-        }
-    }
-
-    ESP_LOGE(TAG, "Invalid requestToChannel call %d", request);
-    return 0;
-}
-
-esp_err_t LineSensor::read(std::vector<uint16_t>& results, bool useCalibration, bool differential) const {
-    int requested = 0;
-    for(int i = 0; i < CHANNELS; ++i) {
-        if(((1 << i) & m_channels_mask) != 0)
-            ++requested;
-    }
-
-    const size_t orig_size = results.size();
-    results.resize(orig_size + requested);
-
-    esp_err_t res = this->read(results.data() + orig_size, useCalibration, differential);
-    if(res != ESP_OK) {
-        results.resize(orig_size);
-        return res;
-    }
-    return ESP_OK;
-}
-
-esp_err_t LineSensor::read(uint16_t *dest, bool useCalibration, bool differential) const {
-    if(!m_installed)
-        return ESP_FAIL;
-
-    int requested = 0;
-    spi_transaction_t transactions[CHANNELS] = { 0 };
-    for(int i = 0; i < CHANNELS; ++i) {
-        if(((1 << i) & m_channels_mask) == 0)
-            continue;
-
-        auto &t = transactions[i];
-        t.user = (void*)requested;
-        t.flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA;
-        t.length = 3 * 8;
-        t.tx_data[0] = 1;
-        t.tx_data[1] = (!differential << 7) | ((i & 0x07) << 4);
-
-        esp_err_t res = spi_device_queue_trans(m_spi, &transactions[i], 100);
-        if(res != ESP_OK)
-            return res;
-        ++requested;
-    }
-
-    if(requested == 0)
-        return ESP_OK;
-
-    spi_transaction_t *trans = NULL;
-    for(int i = 0; i < requested; ++i) {
-        esp_err_t res = spi_device_get_trans_result(m_spi, &trans, portMAX_DELAY);
-        if(res != ESP_OK) {
-            return res;
-        }
-
-        const int idx = (int)trans->user;
-        uint16_t val = ((trans->rx_data[1] & 0x03) << 8) | trans->rx_data[2];
-        if(useCalibration) {
-            const int chan = requestToChannel(i);
-            if(val <= m_calibration.min[chan]) {
-                val = 0;
-            } else {
-                val = int32_t(val - m_calibration.min[chan]) * MAX_VAL / m_calibration.range[chan];
-                val = std::min(MAX_VAL, val);
-            }
-        }
-        dest[idx] = val;
-    }
-    return ESP_OK;
 }
 
 float LineSensor::readLine(bool white_line, float noise_limit, float line_threshold) const {
@@ -200,14 +66,14 @@ LineSensorCalibrator::~LineSensorCalibrator() {
 }
 
 void LineSensorCalibrator::reset() {
-    for(int i = 0; i < LineSensor::CHANNELS; ++i) {
-        m_data.min[i] = LineSensor::MAX_VAL;
+    for(int i = 0; i < Driver::CHANNELS; ++i) {
+        m_data.min[i] = Driver::MAX_VAL;
         m_max[i] = 0;
     }
 }
 
 esp_err_t LineSensorCalibrator::record() {
-    uint16_t vals[LineSensor::CHANNELS];
+    uint16_t vals[Driver::CHANNELS];
     esp_err_t res = m_sensor.read(vals);
     if(res != 0) {
         return res;
@@ -215,7 +81,7 @@ esp_err_t LineSensorCalibrator::record() {
 
     int idx = 0;
     const auto mask = m_sensor.getChannelsMask();
-    for(int i = 0; i < LineSensor::CHANNELS; ++i) {
+    for(int i = 0; i < Driver::CHANNELS; ++i) {
         if(((1 << i) & mask) == 0)
             continue;
 
@@ -230,7 +96,7 @@ esp_err_t LineSensorCalibrator::record() {
 }
 
 void LineSensorCalibrator::save() {
-    for(int i = 0; i < LineSensor::CHANNELS; ++i) {
+    for(int i = 0; i < Driver::CHANNELS; ++i) {
         m_data.range[i] = m_max[i] - m_data.min[i];
     }
     m_sensor.setCalibration(m_data);
--- src/linesensor.h
+++ src/linesensor.h
@@ -1,54 +1,22 @@
 #pragma once
 
 #include <vector>
-#include <atomic>
-
 #include <driver/spi_master.h>
+
+#include "driver.h"
 
 namespace mcp3008 {
 
 class LineSensorCalibrator;
 
 /**
- * \brief The LineSensor device - one MCP3008 chip.
+ * \brief The LineSensor device, based on MCP3008 chip.
  *
  * This class is not thread-safe, you have to make sure the methods are called
  * from one thread at a time only.
- * The install() method has to be called before you can use any other methods.
  */
-class LineSensor {
+class LineSensor : public Driver {
 public:
-    static constexpr int CHANNELS = 8; //!< Amount of channels on the chip
-    static constexpr uint16_t MAX_VAL = 1023; //!< Maximum value returned by from the chip (10bits).
-
-    /**
-     * \brief The LineSensor SPI configuration.
-     */
-    struct Config {
-        Config(gpio_num_t pin_cs = GPIO_NUM_25, gpio_num_t pin_mosi = GPIO_NUM_33,
-            gpio_num_t pin_miso = GPIO_NUM_32, gpio_num_t pin_sck = GPIO_NUM_26,
-            uint8_t channels_mask = 0xFF, int freq = 1350000, spi_host_device_t spi_dev = HSPI_HOST) {
-            this->freq = freq;
-            this->spi_dev = spi_dev;
-            this->channels_mask = channels_mask;
-
-            this->pin_cs = pin_cs;
-            this->pin_mosi = pin_mosi;
-            this->pin_miso = pin_miso;
-            this->pin_sck = pin_sck;
-        }
-
-        int freq; //!< SPI communication frequency
-        spi_host_device_t spi_dev; //!< Which ESP32 SPI device to use.
-        uint8_t channels_mask; //!< Which channels to use, bit mask:
-                               //!< (1 << 0) | (1 << 2) == channels 0 and 2 only.
-
-        gpio_num_t pin_cs;
-        gpio_num_t pin_mosi;
-        gpio_num_t pin_miso;
-        gpio_num_t pin_sck;
-    };
-
     /**
      * \brief The LineSensor's calibration data
      *
@@ -57,38 +25,18 @@ public:
      */
     struct CalibrationData {
         CalibrationData() {
-            for(int i = 0; i < CHANNELS; ++i) {
+            for(int i = 0; i < Driver::CHANNELS; ++i) {
                 min[i] = 0;
-                range[i] = MAX_VAL;
+                range[i] = Driver::MAX_VAL;
             }
         }
 
-        uint16_t min[CHANNELS];
-        uint16_t range[CHANNELS];
+        uint16_t min[Driver::CHANNELS];
+        uint16_t range[Driver::CHANNELS];
     };
 
     LineSensor();
-    virtual ~LineSensor(); //!< The uninstall() method is called from the destructor.
-
-    /**
-     * \brief Initialize the SPI bus. Must be called before any other methods,
-     *        otherwise they will return ESP_FAIL.
-     *
-     * \param cfg the SPI bus configuration.
-     * \return ESP_OK or any error code encountered during the inialization.
-     *         Will return ESP_FAIL if called when already installed.
-     */
-    esp_err_t install(const Config& cfg = Config());
-
-    /**
-     * \brief Free the SPI bus. Must be called last, or other methods return ESP_FAIL.
-     *
-     * \return ESP_OK or any error code encountered during the freeing.
-     *         Will return ESP_FAIL if called when not installed.
-     */
-    esp_err_t uninstall();
-
-    uint8_t getChannelsMask() const { return m_channels_mask; } //!< Get the channel mask, specified in Config::channels_mask
+    virtual ~LineSensor();
 
     /**
      * \brief Start the sensor line calibration procedure.
@@ -113,28 +61,6 @@ public:
     void setCalibration(const CalibrationData& data) { m_calibration = data; }
 
     /**
-     * \brief Read values from the chip. Returns values in range <0; LineSensor::MAX_VAL>.
-     *
-     * \param results the results will be APPENDED to this vector.
-     *        It will be unchanged unless the ESP_OK result is returned (except possibly its capacity).
-     *        Between 0 and LineSensor::CHANNELS values are appended, depending on Config::channels_mask.
-     * \param useCalibration scale sensor values with calibration data. In default, uncalibrated state,
-     *        read returns the same values regardless of this param.
-     *        Use startCalibration() and LineSensorCalibrator to generate the calibration data.
-     * \param differential return differential readings, as specified in the MCP3008 datasheet.
-     * \return ESP_OK or any error code encountered during reading.
-     *         Will return ESP_FAIL if called when not installed.
-     */
-    esp_err_t read(std::vector<uint16_t>& results, bool useCalibration = true, bool differential = false) const;
-
-    /**
-     * \brief See the other read(std::vector<uint16_t>&, bool, bool) const method.
-     *
-     * \param dest array MUST be big enough to accomodate all the channels specified by Config::channels_mask!
-     */
-    esp_err_t read(uint16_t *dest, bool useCalibration = true, bool differential = false) const;
-
-    /**
      * \brief Try to determine a black line's position under the sensors.
      *
      * \param white_line the line is white on black background instead of default black on white background.
@@ -156,13 +82,7 @@ public:
 private:
     LineSensor(const LineSensor&) = delete;
 
-    int requestToChannel(int request) const;
-
     CalibrationData m_calibration;
-    spi_device_handle_t m_spi;
-    spi_host_device_t m_spi_dev;
-    bool m_installed;
-    uint8_t m_channels_mask;
 };
 
 /**
@@ -203,7 +123,7 @@ private:
 
     LineSensor& m_sensor;
     LineSensor::CalibrationData m_data;
-    uint16_t m_max[LineSensor::CHANNELS];
+    uint16_t m_max[Driver::CHANNELS];
 };
 
 }; // namespace mcp3008
