f36b6a8fc2da7c273142d63adf1f62057f1f7ec6
Vojtech Bocek <vbocek@gmail.com>
2019-07-09 10:34:42 +0200

    Add travis CI

---
+++ examples/readchan/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include "mcp3008_linesensor.h"
+
+using namespace mcp3008;
+
+void setup() {
+    LineSensor ls;
+
+    ESP_ERROR_CHECK(ls.install());
+
+    for(int i = 0; i < Driver::CHANNELS; ++i) {
+        printf("%d: %hu\n", i, ls.readChannel(i));
+    }
+
+}
+
+void loop() {
+
+}
